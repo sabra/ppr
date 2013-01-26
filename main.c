ab047ceca5f0056065116f9e0a78c1ef9020eb97
Sabra <sabra091101@gmail.com>
2013-01-26 22:32:43 +0100

    Zalozeni projektu

---
+++ main.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.c
+ * Author: Sabra
+ *
+ * Created on 22. leden 2013, 23:56
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double readFile() {
+    
+}
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    readFile();
+    return (EXIT_SUCCESS);
+}
+
