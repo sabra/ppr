80c5e1ef8621f05528290439a4128bc2088141af
Sabra <sabra091101@gmail.com>
2013-01-29 21:24:31 +0100

    Dokonceno nacitani z CSV souboru

--- main.c
+++ main.c
@@ -11,27 +11,32 @@
 
 #define BUFFER_SIZE 1024
 #define MAX_STRING 32
+#define MAX_LINES 25000
+#define BG "BG"
+#define IG "IG"
+#define TIME "Time"
+#define MOD "Modifier"
 
 FILE *f;
 
-struct data {
-    char time[8];
+typedef struct {
+    char time[9];
     float bg;
     float ig;
     float mod;
-};
-
-char buf[BUFFER_SIZE];
+} DATA;
 
 void parseCSV() {
-    float ig, bg, mod;
-    int igPos, bgPos, modPos;
+    float ig = -1, bg = -1, mod = -1;
+    int timePos = 0, igPos = 0, bgPos = 0, modPos = 0;
     int count = 0;
+    int column = 0;
     int length = 0;
     char *pos = NULL;
     char *first = NULL;
-    int previous = 0;
-    int c;
+    char time[MAX_STRING];
+
+    DATA data[MAX_LINES];
 
     f = fopen("G:\\E\\School\\FAV\\PPR\\semdata\\data\\A0904.csv", "r");
     printf("Opened\n");
@@ -39,107 +44,92 @@ void parseCSV() {
     char line[1024];
     char tmp[500];
     while (fgets(line, BUFFER_SIZE, f) != NULL) {
-        //char* tmp = strdup(line);
         int i;
-
-        //        int header_position[H_MAX] = {-1,-1};// header[0] BG, header[1] IG
-        int h_count = 0; //počet nalezených sloupců
-        char *delimiter = ";\r\n";
-        char *first_char = NULL;
-        char *last_char = NULL;
-        char string[MAX_STRING];
-        char string_bg[MAX_STRING];
-        char string_ig[MAX_STRING];
-        int string_length = 0;
-        int isEmpty = 0; //jestli chybí hodnota IG nebo BG v csv
-        int column = 0; //číslo sloupce
-        int row = 0; //číslo řádku
-
-        /*for (first_char = line; (last_char = strpbrk(first_char, delimiter)) != NULL; 
-                first_char = ++last_char, column++) {
-            string_length = last_char - first_char;
-            strncpy(string, first_char, string_length);
-            string[string_length] = '\0';
-            printf(string);
-            printf("\n");
-        }*/
-
         if (count == 0) {
             first = line;
             pos = line;
+            column = 0;
+           
             for (i = 0; i < strlen(line); i++) {
-                if (line[i] != ';' && line[i] != '\n') {
+            
+                if (line[i] != ';' && line[i] != '\n' && line[i] != ';\r\n') {
                     pos++;
                 } else {
                     length = pos - first;
                     strncpy(tmp, first, length);
-                    if (tmp[length-1] == '\r') {
-                        tmp[length-1] = '\0';
+
+                    if (tmp[length - 1] == '\r') {
+                        tmp[length - 1] = '\0';
                     } else {
                         tmp[length] = '\0';
                     }
+
+                    if (strcmp(tmp, TIME) == 0) {
+                        timePos = column;
+                    } else if (strcmp(tmp, BG) == 0) {
+                        bgPos = column;
+                    } else if (strcmp(tmp, IG) == 0) {
+                        igPos = column;
+                    } else if (strcmp(tmp, MOD) == 0) {
+                        modPos = column;
+                    } else {
+                    }
+
                     first = ++pos;
-                    printf(tmp);
+                    column++;
+                    printf("%s\n", tmp);
                 }
             }
             count++;
         } else {
+            first = line;
+            pos = line;
+            column = 0;
             for (i = 0; i < strlen(line); i++) {
+
                 if (line[i] == ',') {
                     line[i] = '.';
                 }
 
-                if (line[i] != ';') {
+                if (line[i] != ';' && line[i] != '\n' && line[i] != ';\r\n') {
+                    pos++;
+                } else {
+                    length = pos - first;
+                    strncpy(tmp, first, length);
+                    if (tmp[length - 1] == '\r') {
+                        tmp[length - 1] = '\0';
+                    } else {
+                        tmp[length] = '\0';
+                    }
+                    first = ++pos;
 
+                    if (column == timePos) {
+                        strncpy(time, tmp, strlen(tmp) + 1);
+                    } else if (column == bgPos) {
+                        bg = atoff(tmp);
+                    } else if (column == igPos) {
+                        ig = atoff(tmp);
+                    } else if (column == modPos) {
+                        mod = atoff(tmp);
+                    } else {
+                    }
+                    column++;
                 }
+            }
+
+            if (bg != 0 && ig != 0) {
+                strncpy(data[count - 1].time, time, strlen(time));
+                data[count - 1].bg = bg;
+                data[count - 1].ig = ig;
+                data[count - 1].mod = mod;
+                printf("%s %f %f %f\n", data[count - 1].time, data[count - 1].bg,
+                        data[count - 1].ig, data[count - 1].mod);
+                count++;
             }
         }
 
-
-
-        /* FILE *fr;
-        char *delimiter =   ";\r\n";
-        char line[MAX_LINE];
-        int header_position[H_MAX] = {-1,-1};// header[0] BG, header[1] IG
-        int h_count =       0;               //počet nalezených sloupců
-
-        char *first_char = NULL;
-        char *last_char = NULL;
-        char string[MAX_STRING];
-        char string_bg[MAX_STRING];
-        char string_ig[MAX_STRING];
-        int string_length = 0;
-        int isEmpty =       0;            //jestli chybí hodnota IG nebo BG v csv
-        int column =        0;            //číslo sloupce
-        int row =           0;            //číslo řádku
-
-        if ((fr = fopen(PATH, "r")) == NULL) {
-            sprintf(errorMessage, "Soubor %s nelze otevrit ", PATH);           
-            perror(errorMessage);
-            MPI_Finalize();
-            exit(1);
-        }
-
-        while (fgets(line, MAX_LINE, fr) != NULL) {
-            if (h_count != H_MAX) {
-                //rozpoznání pozic sloupců z hlavičky csv
-                for (first_char = line; (last_char = strpbrk(first_char, delimiter)) != NULL; first_char = ++last_char, column++) {
-                    string_length = last_char - first_char;
-                    strncpy(string, first_char, string_length);
-                    string[string_length] = 0;               
-                    //printf("%s\n", string);   //hodnoty zahlavi
-
-                    if (strcmp(string, H_BG) == 0) {
-                        header_position[0] = column;         //pozice 0 je BG
-                        h_count++;
-                        continue;
-                    } else if (strcmp(string, H_IG) == 0) {
-                        h_count++;
-                        header_position[1] = column;
-                    }
-                }
-            }*/
     }
+    printf("Nacteno %i radku", count);
     fclose(f);
 }
 
