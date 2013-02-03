b20e2403115d022028a916055dca57659da1e196
Sabra <sabra091101@gmail.com>
2013-02-03 22:09:13 +0100

    Evolution Header

---
+++ Evolution.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Evolution.h
+ * Author: Sabra
+ *
+ * Created on 3. únor 2013, 21:21
+ */
+
+#ifndef EVOLUTION_H
+#define	EVOLUTION_H
+
+typedef struct _INDIVIDUAL {
+    double ecg;
+    double p;
+    double cg;
+    double c;
+    double k;
+    double dt;
+} INDIVIDUAL, *PINDIVIDUAL;
+
+#endif	/* EVOLUTION_H */
+
