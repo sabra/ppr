9b50ebc226556b38cac92fe8ee9a74ce627ca9f7
Sabra <sabra091101@gmail.com>
2013-02-25 02:21:36 +0100

    Adjustments to main

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ApproxIface.h"
+#include "main.h"
 
 #define BUFFER_SIZE 1024
 #define MAX_STRING 32
@@ -19,13 +20,18 @@
 
 FILE *f;
 LPAPPROXPOINT data;
+int count = 0;
+
+int minB = 100000; //min funkční hodnota b(t)
+int maxB = -100000; //maximální funkční hodnota b(t)
+int minI = 100000; //min funkční hodnota i(t)
+int maxI = -100000; //maximální funkční hodnota i(t), slouží k určení mezí grafu
 
 void parseCSV() {
 
     char time[MAX_STRING];
     float ig = 0, bg = 0, mod = 0;
     int timePos = 0, igPos = 0, bgPos = 0, modPos = 0;
-    int count = 0;
     int column = 0;
     int length = 0;
     char *pos = NULL;
@@ -37,11 +43,9 @@ void parseCSV() {
         exit(1);
     }
 
-    if ((f = fopen("A0904.csv", "r")) == NULL) {
+    if ((f = fopen("I0904.csv", "r")) == NULL) {
         printf("File could not be opened!\n");
         exit(1);
-    } else {
-        printf("Opened\n");
     }
 
     char line[1024];
@@ -55,7 +59,7 @@ void parseCSV() {
 
             for (i = 0; i < strlen(line); i++) {
 
-                if (line[i] != ';' && line[i] != '\n' && line[i] != ';\r\n') {
+                if (line[i] != ';' && line[i] != '\n') {
                     pos++;
                 } else {
                     length = pos - first;
@@ -80,7 +84,6 @@ void parseCSV() {
 
                     first = ++pos;
                     column++;
-                    /*printf("%s\n", tmp);*/
                 }
             }
             count++;
@@ -94,7 +97,7 @@ void parseCSV() {
                     line[i] = '.';
                 }
 
-                if (line[i] != ';' && line[i] != '\n' && line[i] != ';\r\n') {
+                if (line[i] != ';' && line[i] != '\n') {
                     pos++;
                 } else {
                     length = pos - first;
@@ -127,21 +130,54 @@ void parseCSV() {
                 data[count - 1].mod = mod;
                 /*printf("%s %f %f %f\n", data[count - 1].time, data[count - 1].bg,
                          data[count - 1].ig, data[count - 1].mod);*/
+                if (((int) ig) < minI) {
+                    minI = (int) ig;
+                }
+                if (((int) ig) > maxI) {
+                    maxI = (int) ig;
+                }
+
+                if (((int) bg) < minB) {
+                    minB = (int) bg;
+                }
+                if (((int) bg) > maxB) {
+                    maxB = (int) bg;
+                }
                 count++;
             }
         }
 
     }
+
+    count--;
     printf("Nacteno %i radku\n", count);
     fclose(f);
-    printf("File closed\n");
+}
+
+HRESULT GetBuf(LPAPPROXPOINT points, floattype step, int len, int *filled) {
+    // TODO: Upravit, aby sel pouzit i jinej krok
+    if (len * step > count) {
+        printf("You can't read more than you have! Decrease your step!\n");
+        exit(1);
+    } else {
+        int i;
+        for (i = 0; i < len; i++) {
+            int stepTemp = (int) (step * i);
+            points[i].bg = data[stepTemp].bg;
+            points[i].ig = data[stepTemp].ig;
+            *filled = i + 1;
+        }
+    }
+    return 1;
 }
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+    // TODO: Dodelat nacitani parametru z prikazovy radky
     parseCSV();
+    evolution();
     return (EXIT_SUCCESS);
 }
 
---
+++ main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: Sabra
+ *
+ * Created on 24. únor 2013, 13:26
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+extern int count;
+extern int max_dt;
+extern int max_k;
+extern double parametr_p;
+extern double parametr_c;
+extern int filled_parametr_p;
+extern int filled_parametr_c;
+extern long numberOfProcesses;
+
+extern void parseCSV();
+
+#endif	/* MAIN_H */
+
